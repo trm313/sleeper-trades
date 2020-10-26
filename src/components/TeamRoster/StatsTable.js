812c4d37b0006e815e3dafedf312c8b7976a14af
Taylor <trm313@gmail.com>
2020-10-25 20:04:39 -0400

    Added the StatsTable to PlayerPopup. Still has the one stat I can't figure out

--- src/components/TeamRoster/StatsTable.js
+++ src/components/TeamRoster/StatsTable.js
@@ -1,24 +1,78 @@
 import React from "react";
 
 const statsEnum = {
-  1: "A/C",
-  2: "Yds", // Passing
-  3: "TDs", // passing
-  5: "Att", // Rushing
-  6: "Yds", // Rushing
-  13: "TDs", // Rushing
-  14: "Rec", // Receiving
-  15: "Yds", // Receiving
-  20: "TDs", // Receiving
-  21: "",
-  22: "",
+  // 1: "Pass A/C", // Passing
+  2: "Pass Att", // Passing
+  3: "Pass Comp", // passing
+  5: "Pass Yds", //
+  6: "Pass TDs", //
+  13: "?", //
+  14: "Rush Yds", // Rushing
+  15: "Rush TDs", // Rushing
+  20: "Rec", // Receiving
+  21: "Rec Yds", // Receiving
+  22: "Rec TDs", // Receiving
 };
-// Some of thse are wrong...
+
+const filterStatsByPosition = (statsArr, playerPosition) => {
+  let visibilityByPositionEnum = {
+    QB: [2, 3, 5, 6, 13, 14, 15],
+    WR: [13, 14, 15, 20, 21, 22],
+    RB: [13, 14, 15, 20, 21, 22],
+    TE: [13, 14, 15, 20, 21, 22],
+    DEF: [],
+    K: [],
+  };
+
+  let visArr = visibilityByPositionEnum[playerPosition];
+
+  let visibleStats = visArr.map((i) => statsArr.find((s) => s.objKey === i));
+  console.log("filterStatsByPosition", {
+    statsArr,
+    playerPosition,
+    visibleStats,
+    visArr,
+  });
+  return visibleStats;
+};
+
+const compileStatsArr = (statsObj) => {
+  let statsArr = [];
+  for (const [key, value] of Object.entries(statsEnum)) {
+    console.log("compileStats", { key, value });
+    let statItem = {
+      name: statsEnum[key],
+      category: "",
+      value: statsObj[key],
+      objKey: parseInt(key),
+    };
+    statsArr.push(statItem);
+  }
+  return statsArr;
+};
+
+const StatPair = ({ name, value }) => (
+  <div className='flex flex-col items-center justify-center border-r last:border-r-0 border-gray-400 px-2'>
+    <p className='text-sm'>{value}</p>
+    <p className='text-xs'>{name}</p>
+  </div>
+);
 
 const StatsTable = ({ stats, seasonPts, position }) => {
+  if (!stats) {
+    return null;
+  }
+
+  let statsArr = compileStatsArr(stats);
+  let visibleStats = filterStatsByPosition(statsArr, position);
+  console.log("StatsTable", { stats, statsArr, visibleStats });
   return (
     <div className=''>
-      <p>Stats Table</p>
+      <div className='flex p-2'>
+        {visibleStats.map((val, index) => (
+          <StatPair name={val?.name} value={val?.value} />
+        ))}
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /***
  * Styles
- * 
+ *
  * Grays:
  * Background, base = #18202f 2nd darkest)
  * Card = #283245
@@ -8,7 +8,7 @@
  * Light = #3a465b
  * Lighter = #EDF2F9
  * Lightest = #eee
- * 
+ *
  * Gray:
  * 100: #eee
  * 200: #EDF2F9
@@ -19,60 +19,64 @@
  * 800: #090d1c
  */
 
- module.exports = {
-   theme: {
-     fontFamily: {
+module.exports = {
+  theme: {
+    fontFamily: {
       sans: [
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
         '"Segoe UI"',
-        'Roboto',
+        "Roboto",
         '"Helvetica Neue"',
-        'Arial',
+        "Arial",
         '"Noto Sans"',
-        'sans-serif',
+        "sans-serif",
         '"Apple Color Emoji"',
         '"Segoe UI Emoji"',
         '"Segoe UI Symbol"',
         '"Noto Color Emoji"',
       ],
-      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif']
-     },
-     extend: {
-       colors: {
+      serif: ["Georgia", "Cambria", '"Times New Roman"', "Times", "serif"],
+    },
+    extend: {
+      colors: {
         gray: {
-          100: '#fbfbfb',
-          200: '#EDF2F9',
-          300: '#677897',
-          400: '#3a465b',
-          default: '#283245',
-          600: '#283245',
-          700: '#18202f',
-          800: '#090d1c',
+          100: "#fbfbfb",
+          200: "#EDF2F9",
+          300: "#677897",
+          400: "#3a465b",
+          default: "#283245",
+          600: "#283245",
+          700: "#18202f",
+          800: "#090d1c",
         },
         green: {
-          100: '',
-          200: '',
-          300: '',
-          400: '#4ce2a7',
-          500: '',
-          default: '#00b7b3',
-          600: '#00b7b3',
-          700: '',
-          800: '#022047'
-        }
-       },
-       borderRadius: {
-        xl: '1rem',
-        '2xl': '1.5rem'
-       },
-       fontSize: {
-       '2xs': '0.6rem'   
-       },
-       boxShadow: {
-         '2xl': '7px 26px 16px -3px rgba(0, 0, 0, 0.2), 4px 4px 6px -2px rgba(0, 0, 0, 0.15)'
-       }
-     }
-   }
- }
+          100: "",
+          200: "",
+          300: "",
+          400: "#4ce2a7",
+          500: "",
+          default: "#00b7b3",
+          600: "#00b7b3",
+          700: "",
+          800: "#022047",
+        },
+      },
+      borderRadius: {
+        xl: "1rem",
+        "2xl": "1.5rem",
+      },
+      fontSize: {
+        "2xs": "0.6rem",
+      },
+      boxShadow: {
+        "2xl":
+          "7px 26px 16px -3px rgba(0, 0, 0, 0.2), 4px 4px 6px -2px rgba(0, 0, 0, 0.15)",
+      },
+    },
+  },
+  variants: {
+    borderWidth: ["responsive", "last", "hover", "focus"],
+  },
+};
